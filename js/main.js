9738a5a0d3bddd1ef4d89e10f86f68bde96a145e
ian <ians.murray@yahoo.com>
2018-01-07 16:03:34 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
+var cardOne = cards[2];
+cardsInPlay.push(cardOne)
 console.log("User flipped " + cardOne);
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
 console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
