eef0846fad1dd1871e9039ddc47dd96787939b47
ian <ians.murray@yahoo.com>
2018-01-17 00:57:29 -0500

    created game logic, incorporated DOM manip

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ cardImage: "images/king-of-diamonds.png"
 ];
 var cardsInPlay = [];
 
-var checkForMatch = function () {
+var checkForMatch = function (cardId) {
 	if (cardsInPlay.length === 2) {
 			if (cardsInPlay[0] === cardsInPlay[1]) {
 				alert("You found a match!");
@@ -33,14 +33,23 @@ var checkForMatch = function () {
 		}
 };
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var flipCard = function () {
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();	
+	checkForMatch(cardId);	
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+
+};
+
+createBoard();
 
